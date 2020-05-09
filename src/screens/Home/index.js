8624d9d64657b0a612f77fbd432b386fba229a22
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-05-09 15:10:30 -0300

    Creation of the activities area

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,22 +4,24 @@ import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
 
 import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
 
 export default function Home() {
     return (
         <Wrapper>
             <Container>
-            <Header>
-                <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+                <Header>
+                    <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
 
-                <BalanceContainer>
-                    <BalanceTitle>Meu saldo</BalanceTitle>
-                    <Balance>R$0,00</Balance>
-                </BalanceContainer>
-                <AntDesign name="gift" size={30} color="#10c86e" />
-            </Header>
+                    <BalanceContainer>
+                        <BalanceTitle>Meu saldo</BalanceTitle>
+                        <Balance>R$0,00</Balance>
+                    </BalanceContainer>
+                    <AntDesign name="gift" size={30} color="#10c86e" />
+                </Header>
 
-            <Suggestions />
+                <Suggestions />
+                <Activities />
 
             </Container>
         </Wrapper>
